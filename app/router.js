85539895ace89b7961fbe4328de8e1c6f404f76b
Adam Gorbahn and Karen Freeman-Smith <adam+karen@>
2016-10-25 13:14:19 -0700

    make the sidebar

--- app/router.js
+++ app/router.js
@@ -6,6 +6,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('news');
+  this.route('entertainment');
+  this.route('fashion');
+  this.route('lifestyle');
+  this.route('books');
+  this.route('charts');
+  this.route('about');
+  this.route('advertise');
+  this.route('apps');
+  this.route('labs');
+  this.route('terms');
+  this.route('privacy');
+  this.route('dmca');
+  this.route('contact');
 });
 
 export default Router;
---
+++ public/js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  console.log("IT WORK FOR NOW")
+});
